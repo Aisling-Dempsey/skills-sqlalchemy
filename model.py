d08cc8acebdc23e0a8315d1fd69142334ff2ed38
Aisling Dempsey <aisling.n.dempsey@gmail.com>
2016-05-07 12:52:00 -0700

    Working model.py

--- model.py
+++ model.py
@@ -16,14 +16,31 @@ class Model(db.Model):
     """Car model."""
 
     __tablename__ = "models"
-    pass
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    year = db.Column(db.Integer,
+                     nullable=False)
+    brand_name = db.Column(db.String(50))
+    name = db.Column(db.String(50),
+                     nullable=False)
 
 
 class Brand(db.Model):
     """Car brand."""
 
     __tablename__ = "brands"
-    pass
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+    name = db.Column(db.String(50),
+                     nullable=False)
+    founded = db.Column(db.Integer)
+    headquarters = db.Column(db.String(50))
+    discontinued = db.Column(db.Integer)
+
 
 
 # End Part 1
